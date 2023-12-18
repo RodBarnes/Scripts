56c53a714c65087a3a2fb840f95fe06f23b54a12
AdvApp <advappsw@gmail.com>
2023-12-18 14:30:41 -0800

    Added warning notification

--- launch_gateway.sh
+++ launch_gateway.sh
@@ -9,5 +9,13 @@
 # This has been tested on Fedora 39 Cinnamon and works well.
 # It should work under Ubuntu and downstream with little or no changes.
 
-brave-browser $(ip route | grep -Po '(?<=via )(\d{1,3}.){4}')
+# Confirm there is a network connection
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ip route > chk.net
+if [[ -s chk.net ]]; then
+    brave-browser $(ip route | grep -Po '(?<=via )(\d{1,3}.){4}')
+else
+    notify-send -i "$SCRIPT_DIR/warning.png" "Cannot find gateway.  Did you connect to WiFi?"
+fi
