2692e37e6c8cac8cdb4dbcd30666e6f16e993602
AdvApp <advappsw@gmail.com>
2025-04-09 09:59:45 -0600

    Added new scripts for session buddy

---
+++ session_buddy_unzip.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script assumed it is run from ~/Downloads since that is the logical
+# for where the archive will be located.  Especially if using LocalSend.
+
+source /usr/local/lib/colors
+function printx {
+  printf "${YELLOW}$1${NOCOLOR}\n"
+}
+
+STMT=$(basename $0)
+if [[ $# < 1 ]]; then
+  printx "Syntax: $STMT 'filename'\nWhere:  filename is the name of the archive containing the session buddy content"
+  printx "NOTE: It is assumed the archive is located in ~/Downloads\n"
+  exit
+fi
+
+# Obtain the name of the archive
+filename=$1
+
+# Push into the target directory for the Session Buddy database
+# Remove all current content, extract the contents of the archive, then pop back
+pushd /home/$(whoami)/.config/BraveSoftware/Brave-Browser/Default/IndexedDB/chrome-extension_edacconmaakjimmfgnblocblbcdcpbko_0.indexeddb.leveldb
+rm * 2> /dev/null
+unzip ~/Downloads/$filename
+popd
+
---
+++ session_buddy_zip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script assumed it is run from ~/Downloads since that is the logical
+# for where the archive will be located.  Especially if using LocalSend.
+
+# Get the date and create a filename that appends that date
+dt=$(date '+%Y%m%d_%H%M%S');
+filename="sessionbuddy_${dt}.zip"
+
+# Push into the target directory for the Session Buddy database
+# Zip up the contents into an archive that is stored in ~/Downloads, then pop back
+pushd /home/$(whoami)/.config/BraveSoftware/Brave-Browser/Default/IndexedDB/chrome-extension_edacconmaakjimmfgnblocblbcdcpbko_0.indexeddb.leveldb
+zip ~/Downloads/$filename *
+popd
+
