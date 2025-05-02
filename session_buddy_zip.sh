af3ec494fba6e3f4cbcf254fbcb20f4af368308c
AdvApp <advappsw@gmail.com>
2025-05-02 10:54:47 -0600

    Added line to report where the archive was stored

--- session_buddy_zip.sh
+++ session_buddy_zip.sh
@@ -6,10 +6,12 @@
 # Get the date and create a filename that appends that date
 dt=$(date '+%Y%m%d_%H%M%S');
 filename="sessionbuddy_${dt}.zip"
+path=~/Downloads
 
 # Push into the target directory for the Session Buddy database
 # Zip up the contents into an archive that is stored in ~/Downloads, then pop back
 pushd /home/$(whoami)/.config/BraveSoftware/Brave-Browser/Default/IndexedDB/chrome-extension_edacconmaakjimmfgnblocblbcdcpbko_0.indexeddb.leveldb
-zip ~/Downloads/$filename *
+zip $path/$filename *
 popd
 
+echo "Created $path/$filename"
