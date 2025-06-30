ef18b001e1696d1a0e8ac1a043c63be59bbc6f78
AdvApp <advappsw@gmail.com>
2025-06-30 12:21:06 -0600

    New script to report on block devices

---
+++ blkdevinfo.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Show info for all non-removable block devices; aka drives
+
+source /usr/local/lib/colors
+function printx {
+  printf "${YELLOW}$1${NOCOLOR}\n"
+}
+
+STMT=$(basename $0)
+
+# if [[ "$EUID" -ne 0 ]]
+# then
+#   echo 'This must be run as sudo as it relies upon smartctrl to obtain information.'
+#   echo
+# fi
+
+# info=() while IFS= read -r line; do
+# done < <(`sudo smartctl -a /dev/sda`)
+
+showInfo () {
+  printf "/dev/$1\n"
+
+  output=$(sudo smartctl -a /dev/$1)
+  echo "$output" | grep "Device Model"
+  echo "$output" | grep "Model Number"
+  echo "$output" | grep "Serial Number"
+  echo "$output" | grep "Firmware Version"
+  echo "$output" | grep "User Capacity"
+  echo "$output" | grep "Total NVM Capacity"
+  echo "$output" | grep "Form Factor"
+  echo "$output" | grep "SATA Version"
+  echo "$output" | grep "Temperature"
+
+  printf "\n"
+}
+
+lsblk -d -n -oNAME,RM | while read -r name rm; do
+  if [ $rm -eq 0 ]; then
+    showInfo $name
+  fi
+done
