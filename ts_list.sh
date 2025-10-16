74dfc181aa8284371d7245b1ff42fe2fa1558f56
AdvApp <advappsw@gmail.com>
2025-10-15 18:39:35 -0600

    Added command to list backups created using ts_backup

---
+++ ts_list.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# List the backups on the specified drive.
+# Required parameter: <drive> -- the drive to be mounted which contains the backups
+
+source /usr/local/lib/colors
+function printx {
+  printf "${YELLOW}$1${NOCOLOR}\n"
+}
+
+backuppath=/mnt/backup/timeshift/snapshots
+stmt=$(basename $0)
+
+function show_syntax () {
+  printx "Syntax: $stmt <drive>\nWhere:  <drive> is the device containing the backups; e.g., /dev/sdb6"
+  exit  
+}
+
+if [[ $# == 1 ]]; then
+  arg=$1
+  if [ $arg == "?" ] || [ $arg == "-h" ]; then
+    show_syntax
+  else
+    device=$arg
+  fi
+else
+  show_syntax
+fi
+
+if [[ "$EUID" != 0 ]]; then
+  printx "This must be run as sudo.\n"
+  exit
+fi
+
+printx "Listing backup files on $device"
+
+sudo mount -t ext4 $device /mnt/backup
+
+# Get the backups
+unset snapshots
+while IFS= read -r LINE; do
+  snapshots+=("${LINE}")
+done < <( find $backuppath -mindepth 1 -maxdepth 1 -type d | cut -d '/' -f6 )
+
+# Display the backups
+for snapshot in "${snapshots[@]}"; do
+  printf "$snapshot: "
+  if [ -f "$backuppath/$snapshot/timeshift.desc" ]; then
+    printf "$(cat $backuppath/$snapshot/timeshift.desc)\n"
+  else
+    printf "<no desc>\n"
+  fi
+done
+
+sudo umount /dev/sdb6
