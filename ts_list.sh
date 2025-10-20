2f4a5ce2565121f92eb15b21246cab6d85006766
AdvApp <advappsw@gmail.com>
2025-10-20 12:45:52 -0600

    Updated to support mount by label or uuid

--- ts_list.sh
+++ ts_list.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# List the backups on the specified drive.
-# Required parameter: <device> -- the device to be mounted which contains the backups
+# Restore a backup using rsync command as done by TimeShift.
+# One of the followin is required parameter: <device>, <label>, or <uuid> for mounting the device
+# Optional parameter: -t -- Include to do a dry-run
 
 source /usr/local/lib/colors
 function printx {
@@ -9,31 +10,51 @@ function printx {
 }
 
 function show_syntax () {
-  printx "Syntax: $stmt <device>\nWhere:  <device> is the device containing the backups; e.g., /dev/sdb6"
+  printx "Syntax: $stmt <-d <device> | -l <label> | -u <uuid>  [-t] [-s snapshot]"
+  printx "Where:  <device> is the device containing the snapshots; e.g., /dev/sdb6"
+  printx "        [-l <label>] mount the backup device via its filesystem label"
+  printx "        [-u <uuid>] mount the backup devices via the its UUID"
+  printx "        [-t] means to do a test without actually creating the backup; i.e., an rsync dry-run"
+  printx "        [snapshot] is the name (timestamp) of the snapshot to restore."
+  printx "If no snapshot is specified, the device will be queried for the available snapshots."
   exit  
 }
 
 stmt=$(basename $0)
 
+args=("$@")
 if [ $# == 0 ]; then
   show_syntax
 fi
 
-if [[ $# == 1 ]]; then
-  arg=$1
-  if [ $arg == "?" ] || [ $arg == "-h" ]; then
-    show_syntax
-  else
-    # Assume the argument is a device designator
-    device=$arg
+# Analyze the arguments
+for i in "${!args[@]}"; do
+  if [ "-d" == "${args[$i]}" ]; then
+    ((i++))
+    device="${args[$i]}"
+  elif [ "-l" == "${args[$i]}" ]; then
+    ((i++))
+    label="${args[$i]}"
+  elif [ "-u" == "${args[$i]}" ]; then
+    ((i++))
+    uuid="${args[$i]}"
+  elif [ "-s" == "${args[$i]}" ]; then
+    ((i++))
+    snapshotname="${args[$i]}"
+  elif [ "-t" == "${args[$i]}" ]; then
+    dryrun=--dry-run
   fi
-else
-  show_syntax
-fi
+done
 
-if [[ "$EUID" != 0 ]]; then
-  printx "This must be run as sudo.\n"
-  exit
+echo "Device:$device"
+echo "Label:$label"
+echo "UUID:$uuid"
+echo "Dry-run:$dryrun"
+echo "Snapshot:$snapshotname"
+
+# Confirm a backup device was identified
+if [ -z $device ] && [ -z $label ] && [ -z $uuid ]; then
+  show_syntax
 fi
 
 if [ ! -e $device ]; then
@@ -41,11 +62,35 @@ if [ ! -e $device ]; then
   exit
 fi
 
+# if [[ "$EUID" != 0 ]]; then
+#   printx "This must be run as sudo.\n"
+#   exit
+# fi
+
 mountpath=/mnt/backup
 snapshotpath=$mountpath/timeshift/snapshots
 descfile=timeshift.desc
 
-sudo mount -t ext4 $device $mountpath
+# !!!!!!!!!!!!!!!!
+# Before proceeding, the /root filesystem must be mounted -- which can only be done if running from a live image or
+# in some way the /root system is not in use and can be replaced.
+# !!!!!!!!!!!!!!!!
+
+if [ ! -z $device ]; then
+  sudo mount $device $mountpath
+elif [ ! -z $label ]; then
+  sudo mount LABEL=$label $mountpath
+elif [ ! -z $uuid ]; then
+  sudo mount UUID=$uuid $mountpath
+else
+  # It should never be able to get here, but...
+  printx "No device|label|uuid specified."
+fi
+
+if [ $? -ne 0 ]; then
+  printx "Unable to mount the backup device."
+  exit 1
+fi
 
 # Get the snapshots
 unset snapshots
@@ -60,6 +105,7 @@ else
   for snapshot in "${snapshots[@]}"; do
     printf "$snapshot: "
     if [ -f "$snapshotpath/$snapshot/$descfile" ]; then
+      printf "$(sudo du -sh $snapshotpath/$snapshot | awk '{print $1}') -- "
       printf "$(cat $snapshotpath/$snapshot/$descfile)\n"
     else
       printf "<no desc>\n"
@@ -67,4 +113,4 @@ else
   done
 fi
 
-sudo umount $device
+sudo umount $mountpath
