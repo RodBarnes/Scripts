a4a7530b7595b8a8951464c629bc543daa3090d0
AdvApp <advappsw@gmail.com>
2025-10-24 10:32:21 -0600

    Improved check for secure boot

--- ts_restore.sh
+++ ts_restore.sh
@@ -171,13 +171,13 @@ if [ ! -z $snapshotname ]; then
   else
     # Restore the snapshot
     sudo rsync -aAX --delete --exclude-from=/etc/ts_excludes $snapshotpath/$snapshotname/ $restorepath/ > rsync.out
-
     if [ $? -ne 0 ]; then
       printx "Something went wrong with the restore."
       cat rsync.out
       rm rsync.out
       exit 3
     fi
+    rm rsync.out
 
     if [ -f $snapshotpath/$descfile ]; then
       # Delete the description file from the target
@@ -201,17 +201,29 @@ if [ ! -z $snapshotname ]; then
       if [ $? -ne 0 ]; then
         printx "Something went wrong with 'update-grub':"
         cat update-grub.out
-        rm update-grub.out
       fi
+      rm update-grub.out
+
       printx "Installing grub on $restoredevice..."
       sudo chroot $restorepath grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot &> grub-install.out
       if [ $? -ne 0 ]; then
         printx "Something went wrong with 'grub-install':"
         cat grub-install.out
-        rm grub-install.out
       fi
+      rm grub-install.out
 
       printx "Building the UEFI boot entry on $bootdevice with an entry for $restoredevice..."
+
+      # Check Secure Boot status
+      bootfile="grubx64.efi"  # Default for secure boot
+      if sudo mokutil --sb-state &> /dev/null; then
+        # Secure Boot enabled (exit code 0); confirm existence of shimx64.efi
+        if [ -f "$restorepath/boot/efi/EFI/debian/shimx64.efi" ]; then
+          bootfile="shimx64.efi"
+        fi
+      fi
+
+      # Set UEFI boot entry -- where partno is the target partition for the boot entry
       osid=$(grep "^ID=" /etc/os-release | cut -d'=' -f2 | tr -d '"')
       partno="${restoredevice: -1}"
       sudo efibootmgr -c -d $bootdevice -p $partno -L $osid -l "/EFI/$osid/$bootfile.efi" &> /dev/null
