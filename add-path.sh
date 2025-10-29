e1a2dfd39ab1cc13e4bf2724723d477b94b56dbc
AdvApp <advappsw@gmail.com>
2025-10-29 10:04:17 -0600

    No longer needed

--- add-path.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This is intended to be placed in /etc/profile.d for Debian systems
-# so users get access to the system tools.  It should be marked executable.
-export PATH="/usr/local/sbin:/usr/sbin:/sbin:$PATH"
