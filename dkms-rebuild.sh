fc2e13a6b45298010ffb4ad1d906a093b088ac8e
AdvApp <advappsw@gmail.com>
2025-11-11 09:17:31 -0700

    Revised and expanded comments

--- dkms-rebuild.sh
+++ dkms-rebuild.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+# This script was built from a conversation with Grok (https://grok.com/c/3d149a35-ac28-4ba2-aa5c-855ab9bf2dde)
+# on the issue around DKMS files not being included as part of the kernel build.  I experienced this with
+# 6.8.0-50 and later with 6.11.0-28.  The conversation makes clear this is not unique to my systems and is a knonw
+# issue with how Ubuntu was building the kernel.  This script addresses that by taking the steps needed to ensure
+# that the DKMS modules are built and included in the initramfs.
+#
+# The question now, is whether to make it an automatic script in /etc/kernel/postinst.d (e.g., zz-dkms-rebuild),
+# leave it as a manual step that can be run when it is determined to be necessary, or some combination; i.e.,
+# have it check for the existence of the .ko files and if not found, proceed to build them.
+#
+# As this is currently written, it is pegged to specific versions of the DKMS and eliminates all other versions.
+# Going forward that would have to be changed as these are built if a new version comes along and is to replace
+# this pegged "base" version.
+
 set -e
 
 build_module() {
@@ -57,8 +72,6 @@ build_module() {
   fi
 }
 
-# If this runs only as a post install step then this can just be a local variable in the function
-# But maybe it makes more sense to have it be able to be specified so that it can be run as needed?
 the_kernel=$(uname -r)
 
 build_module "nvidia" "580.95.05" $the_kernel
